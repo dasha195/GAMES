ffd7057ab251992f9f58f6a8db1df09399405617
Dasha Topalova <daria_topa174@mail.ru>
2024-12-20 13:18:33 +0500

    Исправление

--- GAMES/GAMES/Form3.cs
+++ GAMES/GAMES/Form3.cs
@@ -30,10 +30,7 @@ namespace GAMES
             foreach (Button b in panel1.Controls) /*активируем все кнопки на панели(делаем их доступными для нажатия).*/
                 b.Enabled = true;
 
-           // Image orginal = Image.FromFile(@"img\img.jpg");
-
-            string imagePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "img.jpg");
-            Image orginal = Image.FromFile(@"img\img.jpg"); // Загружаем изображение
+            Image orginal = Image.FromFile(@"..\..\Resources\img\img.jpg"); // Загружаем изображение
        
 
             cropImageTomages(orginal, 270, 270); /*разрезаем изображение на 8 кусочков(270x270 — размер изображения).*/
